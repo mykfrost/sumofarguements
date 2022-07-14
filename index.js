52bb99a301bc357c5bba146f657f8a17ebd0929c
Michael Okello <okellofrost@gmail.com>
2022-07-14 13:03:18 +0300

    Function of sum that takes an aray & items

---
+++ index.js
@@ -0,0 +1,13 @@
+//Create fucntion sum that takes args and returns its sum
+
+//Array.isArray()
+
+console.log(sum(1,2,3,4));
+
+if(items.lenght === 1 && Array.isArray(itens[0]))
+items = [...items[0]];
+
+function sum(...items){
+return  items.reduce((a,b) => a+ b);
+
+}
